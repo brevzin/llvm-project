8e72f8290b7e3e0718e830bd06c33f85d4294d9d
Barry Revzin <brevzin@jumptrading.com>
2025-12-29 16:38:47 -0600

    More interesting test case

--- clang/test/SemaCXX/template-string-test.cpp
+++ clang/test/SemaCXX/template-string-test.cpp
@@ -270,8 +270,8 @@ void test_macro_expansion() {
 
   auto answer = []{ return 42; };
   #define RES (answer())
-  auto s26 = t"a={SINGLE} b={MULTIPLE} c={RES}";
-  static_assert(__builtin_strcmp(s26.fmt, "a={} b={} c={}") == 0);
+  auto s26 = t"{SINGLE=} {MULTIPLE=} {RES = }";
+  static_assert(__builtin_strcmp(s26.fmt, "SINGLE={} MULTIPLE={} RES = {}") == 0);
   static_assert((^^decltype(s26._0)) == (^^int));
   static_assert((^^decltype(s26._1)) == (^^int));
   static_assert((^^decltype(s26._2)) == (^^int));
